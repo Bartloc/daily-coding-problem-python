e6a0165b7ee554c58f5a90837257947f80c596b4
Bartloc <58297109+Bartloc@users.noreply.github.com>
2019-12-08 10:52:11 +0100

    Create solution-029.py

---
+++ solution-029.py
@@ -0,0 +1,36 @@
+def enc(stri):
+    if not stri:
+        return None
+    temp = stri[0]
+    outstri = ''
+    enc = 0
+    for s in stri:
+        if s == temp:
+            enc = enc+1
+        else:
+            outstri = outstri + str(enc)+temp
+            temp = s
+            enc = 1
+    outstri = outstri + str(enc)+temp
+    return(outstri)
+
+
+def dec(stri):
+    outstri = ''
+    numb = ''
+    s = 0
+    if not stri:
+        return None
+    for s in stri:
+        num = s
+        if num.isdigit():
+            numb = numb+num
+        else:
+            temp = s * int(numb)
+            outstri = outstri + temp
+            numb = ''
+    return outstri
+
+
+assert enc('AAAABBBCCDAA')=='4A3B2C1D2A'
+assert dec('4A3B2C1D2A')=='AAAABBBCCDAA'
