127c3063172b5a87a15fa18ebf21d9a2b111570e
Bartloc <58297109+Bartloc@users.noreply.github.com>
2019-12-11 07:22:21 +0100

    Update solution-231.py

--- solution-231.py
+++ solution-231.py
@@ -5,13 +5,14 @@ def rearrange(s):
     for c1,c2 in Counter(s).most_common():
         str2,str1 =(str2+c2*c1,str1)  if len(str1)+c2>len(str2) else (str2, str1+c2*c1) 
     str1,str2 = (str2,str1) if len(str2)>len(str1) else (str1,str2)
-
+    a=0
     if len(str1)-len(str2)>1:
+       
         a=(len(str1)-len(str2))//2
         str2=str2+str1[:a]
         str1=str1[a:]
 
-    if str1[-1]!=str2[-1]:
+    if str1[-1]!=str2[-a]:
         for i,x in enumerate(str1):
             str3 +=str1[i]
             if i<len(str2):
