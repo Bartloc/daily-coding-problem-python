b2c7ec5ba0621aaa60055136cddf4282ea2a5e5b
Bartloc <58297109+Bartloc@users.noreply.github.com>
2019-12-12 12:26:56 +0100

    Update solution-221.py

--- solution-221.py
+++ solution-221.py
@@ -1,4 +1,5 @@
-nish =lambda n,num=7: sum(pow(num,i) for (i,x) in enumerate(bin(n)[2:][::-1]) if int(x)==1)
+nish =lambda n,num=7: sum(pow(num,i) for (i,x) in enumerate(bin(n)[2:][::-1]) if int(x)==1)\
+      if n>0 else 0
 
 
 #testing 
